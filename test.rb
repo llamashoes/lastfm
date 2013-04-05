4e45c3eb969b46ea6a7b124528847e778b280e13
llamashoes <rgifford@gmail.com>
2013-04-05 14:17:23 -0600

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,8 +1,8 @@
 require 'lastfm_lib'
 
 #LastFM Key
-key = "05b13c72c513d9a32b8e574283f75146"
-username = "llamashoes" #can be ar ARGV later on
+key = "<key here>"
+username = "<username here>" #can be ar ARGV later on
 
 lfm = LastFm.new
 lfm.ListAllTracks(key, username)
